510a863c3fa34de2ebc003faf720542ecc3e13cb
root <root@localhost>
2024-03-03 14:47:28 -0700

    Initial commit

---
+++ src/com.aaappps.myapp/MainActivity.java
@@ -0,0 +1,12 @@
+package com.aaappps.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }  
+}
